76cc75ecad4566f51e4c74830a8e1fdb94c81cc5
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-17 15:06:48 +0400

    Add score feature

--- pipes.gd
+++ pipes.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 var speed = 75
 
+signal score
+
 func _process(delta):
 	position.x -= speed * delta
 
-
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_score_area_body_entered(body):
+	print(body)
+	score.emit()
--- scenes/main.gd
+++ scenes/main.gd
@@ -2,8 +2,11 @@ extends Node
 @onready var player = $Player
 @onready var game_over = $GUI/GameOver
 @onready var start_game = $GUI/StartGame
+@onready var score_text = $GUI/Label
 
 @export var pipes: PackedScene
+
+var score_value = 0 
 
 func _ready():
 	game_over.hide()
@@ -12,19 +15,25 @@ func _ready():
 func spawn_pipe():
 	if not start_game.visible:
 		var pipe = pipes.instantiate()
+		pipe.connect("score", _on_player_score)
 		pipe.position.y = randi_range(-184,-10)
 		add_child(pipe)
 #
 func _on_timer_timeout():
 	spawn_pipe()
 
-
 func _on_player_touch():
 	game_over.show()
-	
+
+func _on_player_score():
+	print(score_text)
+	score_value += 1
+	score_text.text = "Score: %s" % score_value
+
 func _unhandled_input(event):
 	if event.is_action_pressed("jump") and game_over.visible:
 		get_tree().reload_current_scene()
 	if event.is_action_pressed("jump") and start_game.visible:
 		start_game.hide()
 		player.set_physics_process(true)
+
