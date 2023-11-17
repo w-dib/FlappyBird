9150e8f36b88b130bc048c00e728eaed21fb2c92
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-17 15:25:54 +0400

    Finishing touches

--- player.gd
+++ player.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 @onready var move = $Move
 @onready var death = $Death
+@onready var player = $Sprite2D
 
 const JUMP_VELOCITY = -400.0
 const gravity = 980
+
 var tween
 
 signal touch
 
 func _physics_process(delta):
-		velocity.x = 0
 		velocity.y += gravity * delta
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
@@ -20,8 +21,13 @@ func _physics_process(delta):
 
 
 func _on_area_2d_body_entered(_body):
+	collision_layer = 0
+	collision_mask = 0
+	set_physics_process(false)
 	death.play()
 
 func _on_death_finished():
 	touch.emit()
+	collision_layer = 1
+	collision_mask = 1
 	queue_free()
