dafb08ea37a4ba4754a949bf8e500735bda9cac7
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-17 12:50:14 +0400

    Add functional UI for start and game over

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,16 +1,20 @@
 extends Node
 @onready var player = $Player
 @onready var game_over = $GUI/GameOver
+@onready var start_game = $GUI/StartGame
 
 @export var pipes: PackedScene
 
 func _ready():
 	game_over.hide()
+	player.set_physics_process(false)
+#	player.hide()
 
 func spawn_pipe():
-	var pipe = pipes.instantiate()
-	pipe.position.y = randi_range(-184,-10)
-	add_child(pipe)
+	if not start_game.visible:
+		var pipe = pipes.instantiate()
+		pipe.position.y = randi_range(-184,-10)
+		add_child(pipe)
 #
 func _on_timer_timeout():
 	spawn_pipe()
@@ -20,3 +24,7 @@ func _on_player_touch():
 	game_over.show()
 	player.queue_free()
 	
+func _unhandled_input(event):
+	if event.is_action_pressed("jump") and start_game.visible:
+		start_game.hide()
+		player.set_physics_process(true)
